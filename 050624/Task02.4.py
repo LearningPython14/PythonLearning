25f193d0c5dd325802fbddde347ba275d1d0c28f
learning Python <learningpython14@gmail.com>
2024-06-20 19:40:02 +0530

    task 2 commit

--- 050624/Task02.4.py
+++ 050624/Task02.4.py
@@ -3,3 +3,11 @@
 # n = 7
 # 0, 1, 2, 3, 5, 8, 13
 
+#https://sdet.club/events/265cfca9-0841-49ce-8686-69b026818827
+
+
+#https://sdet.club/events/265cfca9-0841-49ce-8686-69b026818827
+#https://github.com/PramodDutta/Py3xATBLearning.git
+
+#https://github.com/PramodDutta/Py3xATBLearning.git
+
